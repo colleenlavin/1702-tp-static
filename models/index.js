fdba69cbd078596517d4dbc40f7a7fcdd416b7de
Rachel Cohen <rfc@alumni.duke.edu>
2017-03-17 15:44:49 -0400

    root route set up

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/tripplanner',{   
-});
+var db = new Sequelize('postgres://localhost:5432/tripplanner');
+
 var Place = db.define('place',{
     address:{type: Sequelize.STRING},
     city:{type: Sequelize.STRING},
@@ -8,9 +8,10 @@ var Place = db.define('place',{
     phone: {type: Sequelize.STRING},
     location:{type: Sequelize.ARRAY(Sequelize.FLOAT)}
 })
+
 var Hotel = db.define('hotel',{
     name:{type: Sequelize.STRING},
-    num_stars:{type: Sequelize.STRING},
+    num_stars:{type: Sequelize.FLOAT, validate: {min: 1, max: 5}},
     amenities: {type: Sequelize.STRING}
 })
 var Activity = db.define('activity',{
@@ -20,5 +21,17 @@ var Activity = db.define('activity',{
 var Restaurant = db.define('restaurant',{
     name:{type: Sequelize.STRING},
     cuisine:{type: Sequelize.STRING},
-    price: {type: Sequelize.STRING}
+    price: {type: Sequelize.INTEGER, validate: {min: 1, max: 5}}
 })
+
+Hotel.belongsTo(Place);
+Restaurant.belongsTo(Place);
+Activity.belongsTo(Place);
+
+module.exports = {
+    Place: Place,
+    Hotel: Hotel,
+    Activity: Activity,
+    Restaurant: Restaurant,
+    db: db
+};
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,28 @@
 var Promise = require('bluebird');
+var Hotel = require('../models').Hotel;
+var Restaurant = require('../models').Restaurant;
+var Activity = require('../models').Activity;
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+var outerScopeContainer = {};
+Hotel.findAll()
+.then(function (dbHotels) {
+  outerScopeContainer.dbHotels = dbHotels;
+  return Restaurant.findAll();
+})
+.then(function (dbRestaurants) {
+  outerScopeContainer.dbRestaurants = dbRestaurants;
+  return Activity.findAll();
+})
+.then(function (dbActivities) {
+  res.render('index', {
+    templateHotels: outerScopeContainer.dbHotels,
+    templateRestaurants: outerScopeContainer.dbRestaurants,
+    templateActivities: dbActivities
+  });
+})
+.catch(next);
+})
+
